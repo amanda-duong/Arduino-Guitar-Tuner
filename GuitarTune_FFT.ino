4b1faf95cf31c616c1bb2bd3146c7fa7136cd125
amanda-duong <amandaduong22@gmail.com>
2026-07-31 09:49:39 -0700

    Added more comments

--- GuitarTune_FFT.ino
+++ GuitarTune_FFT.ino
@@ -4,13 +4,13 @@ const int leftRedLED = 5;
 const int greenLED = 4;
 const int rightRedLED = 3;
 
-// Initializing microphone calibration parameters
+// Define microphone calibration parameters
 // MIC_OFFSET: Center / Baseline of signal oscillation
 // VOL_THRESHOLD: Baseline volume to begin analyzing frequency
 const int MIC_OFFSET = 335; 
 const int VOL_THRESHOLD = 30;  
 
-// Initializing timing parameters
+// Define timing parameters
 // IDLE_TIME: Time (ms) before LEDs turn off due to no note detected
 // PLUCK_DELAY: Short delay (ms) after pluck for vibrations to normalize before signal is analyzed
 const int IDLE_TIME = 3000;
@@ -21,7 +21,7 @@ const double TOLERANCE = 3;
 
 #include <arduinoFFT.h>
 
-// Initializing expected frequencies and names for "in tune" guitar strings
+// Define expected frequencies (Hz) and names for "in tune" guitar strings
 // *currently designed only for standard tuning (EADGBE)
 const double notes[6] =
 {
@@ -48,32 +48,35 @@ void setup()
 {
   Serial.begin(115200);
 
-  // Establish LED pins as outputs
+  // Configure LED pins as outputs
   pinMode(leftRedLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
   pinMode(rightRedLED, OUTPUT);
 }
 
-// Initializing FFT parameters
-// Larger number of samples improves accuracy, but takes longer to record and process
+// Define FFT and FFT-related timing parameters
+// samples: number of samples used in each FFT frequency analysis
+// samplingFrequency: number of samples collected per second
+// samplingPeriod: time (microseconds) between collected samples
+// lastPluckTime: when the last string was plucked
 const uint16_t samples = 512;
 const double samplingFrequency = 2000;
+unsigned long samplingPeriod = round(1000000.0 / samplingFrequency);
+unsigned long lastPluckTime = 0;
 
+// Initialize arrays for FFT processing
+// vReal: stores collected audio sample data
+// vImag: stores imaginary helper data
 double vReal[samples];
 double vImag[samples];
 
-// Initialize FFT object for conversion of raw data to frequency information
+// Initialize FFT object for processing collected audio samples to determine frequency
 ArduinoFFT<double> FFT(vReal, vImag, samples, samplingFrequency);
-
-int previous = 0;
-unsigned long prevTime = 0;
-unsigned long lastPluckTime = 0;
 
 void loop() 
 {
-
+  // Determine if string was plucked and turn of LEDs if inactive
   int vol_level = abs(analogRead(mic) - MIC_OFFSET);
-
   if (vol_level < VOL_THRESHOLD)
   {
       if (millis() - lastPluckTime > IDLE_TIME)
@@ -82,15 +85,14 @@ void loop()
         digitalWrite(greenLED, LOW);
         digitalWrite(rightRedLED, LOW);
       }
-      return;   // Nothing has been plucked yet
+      return; 
   }
 
   lastPluckTime = millis();
 
   delay(PLUCK_DELAY);
 
-  unsigned long samplingPeriod = round(1000000.0 / samplingFrequency);
-
+  // collect samples
   for (int i = 0; i < samples; i++)
   {
       unsigned long start = micros();
@@ -100,25 +102,30 @@ void loop()
 
       while (micros() - start < samplingPeriod)
       {
-          // wait
+          // wait to ensure full sampling period has elapsed
       }
   }
 
+  // calculate average
   double mean = 0;
   for (int i = 0; i < samples; i++) mean += vReal[i];
   mean /= samples;
 
+  // center signal values around 0
   for (int i = 0; i < samples; i++) vReal[i] -= mean;
 
-
+  // Perform FFT analysis  
   FFT.windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
   FFT.compute(FFT_FORWARD);
   FFT.complexToMagnitude();
 
+  // Define peak detection parameters
+  // threshold: minimum magnitude of a valid frequency
+  // lowestFreq: stores lowest valid detected frequency
   double threshold = 80.0;
-
   double lowestFreq = 10000;
 
+  // Determine lowest significant frequency peak (fundamental frequency)
   for (int i = 1; i < (samples / 2); i++)
   {
       double freq = (i * samplingFrequency) / samples;
